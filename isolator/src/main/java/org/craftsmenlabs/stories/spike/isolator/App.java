27e8b421776b9bef3be4bc5d988a8f40b674c45e
Kevin van Ingen <kevin.vaningen@codecentric.nl>
2016-10-06 10:38:19 +0200

    Added structure

---
+++ isolator/src/main/java/org/craftsmenlabs/stories/spike/isolator/App.java
@@ -0,0 +1,13 @@
+package org.craftsmenlabs.stories.spike.isolator;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+
+}
