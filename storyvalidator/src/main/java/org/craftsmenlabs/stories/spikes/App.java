ba3e07bf669449e2a0185f7f5f4aeb2348205a49
Kevin van Ingen <kevin.vaningen@codecentric.nl>
2016-10-06 10:34:15 +0200

    Added structure

---
+++ storyvalidator/src/main/java/org/craftsmenlabs/stories/spikes/App.java
@@ -0,0 +1,13 @@
+package org.craftsmenlabs.stories.spikes;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+
+}
