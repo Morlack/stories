fc0230955c1a4d0b079764d4db8ee26e7897bb1f
Kevin van Ingen <kevin.vaningen@codecentric.nl>
2016-10-20 11:57:20 +0200

    Reworked jira importer.

--- importer/src/main/java/org/craftsmenlabs/stories/spike/importer/JiraAPIImporter.java
+++ importer/src/main/java/org/craftsmenlabs/stories/spike/importer/JiraAPIImporter.java
@@ -10,89 +10,98 @@ import org.slf4j.LoggerFactory;
  */
 public class JiraAPIImporter implements Importer
 {
-	static final URL SOURCE_PATHS = JiraAPIImporter.class.getResource("logback.xml");
-
 	public static final int CONNECTION_TIMEOUT = 5000;
 	public static final int DOWNLOAD_TIMEOUT = 60000;
-
+	//static final URL SOURCE_PATHS = JiraAPIImporter.class.getResource("logback.xml");
 	private final Logger logger = LoggerFactory.getLogger(JiraAPIImporter.class);
-
-	private HttpURLConnection conn;
 
 	private String urlResource;
 	private String projectKey;
 	private String authKey;
 	private String statusKey;
 
-    public JiraAPIImporter(String urlResource, String projectKey, String authKey, String statusKey) {
-        this.urlResource = urlResource;
-        this.projectKey = projectKey;
-        this.authKey = authKey;
-        this.statusKey = statusKey;
-    }
+	public JiraAPIImporter(String urlResource, String projectKey, String authKey, String statusKey)
+	{
+		this.urlResource = urlResource;
+		this.projectKey = projectKey;
+		this.authKey = authKey;
+		this.statusKey = statusKey;
+	}
 
-    public String getDataAsString()
+	public String getDataAsString()
 	{
 		String returnsResponse = "";
 		try
 		{
-			URL url = new URL(urlResource
-				+ "/rest/api/2/search?jql="
-				+ "project%3D" + httpEncode(projectKey) + "+AND+"
-				+ "type%3DStory+AND+"
-				+ "status%3D\"" + httpEncode(statusKey) + "\""
-				+ "&maxResults=100000");
-			logger.info("Retrieving data form:" + url.toString());
+			URL url = createUrl(projectKey, statusKey);
 
-			conn = (HttpURLConnection)url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-			conn.setRequestProperty("Content-Type", "application/json");
-			conn.setRequestProperty("Authorization", "Basic " + authKey);
-			conn.setConnectTimeout(CONNECTION_TIMEOUT);
-			conn.setReadTimeout(DOWNLOAD_TIMEOUT);
+			logger.info("Retrieving data form:" + url.toString());
+			HttpURLConnection conn = createConnection(url, authKey);
 
 			//execute call
 			if (conn.getResponseCode() != 200)
 			{
+				logger.error("Failed : HTTP error code : " + conn.getResponseCode() + "\n" + conn
+					.getResponseMessage());
 				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode() + "\n" + conn
 					.getResponseMessage());
+
 			}
 
-			// Buffer the result into a string
-			BufferedReader rd = new BufferedReader(
-				new InputStreamReader(conn.getInputStream()));
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while ((line = rd.readLine()) != null)
-			{
-				sb.append(line);
-			}
-			rd.close();
-
-			returnsResponse = sb.toString();
+			returnsResponse = readFromUrlConnection(conn);
 
 			conn.disconnect();
 
 		}
-		catch (MalformedURLException e)
-		{
-
-			e.printStackTrace();
-
-		}
 		catch (IOException e)
 		{
-
-			e.printStackTrace();
-
+			logger.error("Unable to create Jira Connection using URL: " + urlResource, e);
+			throw new RuntimeException();
 		}
 		//writeToFile("tmp.json",returnsResponse.toString());
 
 		return returnsResponse;
 	}
 
-	public void writeToFile(String filename, String data)
+	protected URL createUrl(String projectKey, String statusKey) throws UnsupportedEncodingException, MalformedURLException
+	{
+		return new URL(urlResource
+			+ "/rest/api/2/search?jql="
+			+ "project%3D" + httpEncode(projectKey) + "+AND+"
+			+ "type%3DStory+AND+"
+			+ "status%3D\"" + httpEncode(statusKey) + "\""
+			+ "&maxResults=100000");
+	}
+
+	protected HttpURLConnection createConnection(URL url, String authToken) throws IOException
+	{
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Authorization", "Basic " + authToken);
+		conn.setConnectTimeout(CONNECTION_TIMEOUT);
+		conn.setReadTimeout(DOWNLOAD_TIMEOUT);
+		return conn;
+	}
+
+	protected String readFromUrlConnection(URLConnection conn) throws IOException
+	{
+		// Buffer the result into a string
+		BufferedReader rd = new BufferedReader(
+			new InputStreamReader(conn.getInputStream()));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null)
+		{
+			sb.append(line);
+		}
+		rd.close();
+
+		return sb.toString();
+	}
+
+	protected void writeToFile(String filename, String data)
 	{
 		File f = new File("tmp.json");
 		try (FileOutputStream out = new FileOutputStream(filename))
--- importer/src/test/java/org/craftsmenlabs/stories/spike/importer/JiraAPIImporterTest.java
+++ importer/src/test/java/org/craftsmenlabs/stories/spike/importer/JiraAPIImporterTest.java
@@ -3,20 +3,24 @@ package org.craftsmenlabs.stories.spike.importer;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+import java.net.*;
+import mockit.*;
 
 /**
  *
  */
 public class JiraAPIImporterTest
 {
-//	@Tested
+	//@Tested
 	JiraAPIImporter _jiraAPIImporter;
 
-	@Test
+	@Test(expected = RuntimeException.class)
 	public void testimportFrom() throws Exception
 	{
 		_jiraAPIImporter = new JiraAPIImporter("http://foo.bar", "1", "2", "To Do");
 		String dataImport = _jiraAPIImporter.getDataAsString();
 		assertThat(dataImport).contains("");
+
 	}
 }
